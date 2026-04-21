cdb1e9e36253f2262df5a3b42d97d02aad19ec89
TaejunRyu <ryuwillow@gmail.com>
2026-04-22 01:06:01 +0900

    NVS  컴파일 가능하도록 수정

---
+++ main/include/ryu_nvs.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <nvs_flash.h>
+#include <nvs.h>
+
+
+/**
+ * @brief NVS에 저장할 항목정리
+ *  1. IMU의 Calibration 정보
+ *      1.1 acc  offset
+ *      1.2 gyro offset
+ *      1.3 mag  offset
+ *  2. pid 정보
+ * 
+ *  3. qgc와 연결되어진 변수 
+ * 
+ *  4.
+ * 
+ */
+namespace NVS
+{
+inline constexpr char NVS_NAMESPACE[] = "QGC_PARAMS";
+
+struct SensorCalibration {
+    float acc_main_offset[3];   // ICM20948
+    float acc_sub_offset[3];    // ICM20948
+    float gyro_main_offset[3];  // ICM20948
+    float gyro_sub_offset[3];   // ICM20948
+    float mag_main_offset[3];   // IST8310 Hard-iron 보정값
+    float mag_sub_offset[3];    // AK09916 Hard-iron 보정값
+    bool is_calibrated;  // 캘리브레이션 완료 여부 플래그
+};
+
+}//namespace NVS
+
--- main/ryu_nvs.cpp
+++ main/ryu_nvs.cpp
@@ -1,40 +1,12 @@
-#include <nvs_flash.h>
-#include <nvs.h>
+#include "ryu_nvs.h"
 #include "ryu_paramtable.h"
-
-/**
- * @brief NVS에 저장할 항목정리
- *  1. IMU의 Calibration 정보
- *      1.1 acc  offset
- *      1.2 gyro offset
- *      1.3 mag  offset
- *  2. pid 정보
- * 
- *  3. qgc와 연결되어진 변수 
- * 
- *  4.
- * 
- */
 
 namespace NVS
 {
 
-inline constexpr char NVS_NAMESPACE[] = "QGC_PARAMS";
-
-struct SensorCalibration {
-    float acc_main_offset[3];   // ICM20948
-    float acc_sub_offset[3];    // ICM20948
-    float gyro_main_offset[3];  // ICM20948
-    float gyro_sub_offset[3];   // ICM20948
-    float mag_main_offset[3];   // IST8310 Hard-iron 보정값
-    float mag_sub_offset[3];    // AK09916 Hard-iron 보정값
-    bool is_calibrated;  // 캘리브레이션 완료 여부 플래그
-};
-
-
 // [저장] 특정 인덱스의 파라미터 정보 저장 (Pointer 제외)
 esp_err_t save_param_struct_to_nvs(int index) {
-    using  namespace ParamTable;
+    using  namespace PARAM;
     nvs_handle_t my_handle;
     esp_err_t err = nvs_open(NVS_NAMESPACE, NVS_READWRITE, &my_handle);
     if (err != ESP_OK) return err;
@@ -50,9 +22,13 @@ esp_err_t save_param_struct_to_nvs(int index) {
         uint8_t type;
     } temp_data;
 
-    drone_params[index].name.copy(temp_data.name, 16); // std::string_view에서 char 배열로 복사
-    temp_data.value = drone_params[index].value;
-    temp_data.type = drone_params[index].type;
+    
+    memcpy(temp_data.name,params[index].name.data(),16); // std::string_view에서 char 배열로 복사
+
+    float *param_ptr =  reinterpret_cast<float*>(&values);
+
+    temp_data.value = param_ptr[index];
+    temp_data.type  =  params[index].type;
 
     // Binary Blob으로 저장
     err = nvs_set_blob(my_handle, key, &temp_data, sizeof(temp_data));
@@ -64,11 +40,11 @@ esp_err_t save_param_struct_to_nvs(int index) {
 
 // [로드] 부팅 시 NVS에서 값을 읽어와 pointer와 매칭
 void load_params_struct_from_nvs() {
-    using namespace ParamTable;
+    using namespace PARAM;
     nvs_handle_t my_handle;
     if (nvs_open(NVS_NAMESPACE, NVS_READONLY, &my_handle) != ESP_OK) return;
 
-    for (int i = 0; i < praram_count; i++) {
+    for (int i = 0; i < count; i++) {
         char key[16];
         snprintf(key, sizeof(key), "p_%d", i);
 
@@ -81,11 +57,9 @@ void load_params_struct_from_nvs() {
         size_t required_size = sizeof(temp_data);
         if (nvs_get_blob(my_handle, key, &temp_data, &required_size) == ESP_OK) {
             // 1. 값 업데이트
-            drone_params[i].value = temp_data.value;
+            float *param_ptr =  reinterpret_cast<float*>(&values);
+            param_ptr[i] = temp_data.value;
             // 2. 연결된 실제 변수(Pointer)가 있다면 동기화 (가장 중요)
-            if (drone_params[i].pointer != NULL) {
-                memcpy(drone_params[i].pointer, &temp_data.value, 4);
-            }
         }
     }
     nvs_close(my_handle);
@@ -94,7 +68,7 @@ void load_params_struct_from_nvs() {
 
 
 inline constexpr char NVS_MAIN_IMU_OFFSET[] = "MAINIMU_OFFSET";
-inline constexpr char NVS_MAIN_IMU_OFFSET[] = "SUBIMU_OFFSET";
+inline constexpr char  NVS_SUB_IMU_OFFSET[] = "SUBIMU_OFFSET";
 // NVS에 오프셋 저장
 esp_err_t save_offsets_to_nvs(char *nvs_name,const std::array<float, 3>& acc, const std::array<float, 3>& gyro) {
     nvs_handle_t my_handle;
