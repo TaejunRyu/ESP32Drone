20af8f74347a091ba17c7eb9b6a176dcca7d820d
TaejunRyu <ryuwillow@gmail.com>
2026-05-01 20:09:31 +0900

    aa

--- test/test.cpp
+++ test/test.cpp
@@ -1,15 +1,23 @@
-#include <stdio.h>
+#include "stdio.h"
 
-struct mainframe{
-    int aa;
-    int bb;
+struct aaa{
+    int a;
+    int b;
 };
 
+int  main(void) {
+    aaa test[4] = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
+    struct aaa* ptr = &test[0];
 
-int main(){
-    mainframe kk;
-    kk.aa = 1;
-    kk.bb = 1;
+    int c = (test+0)->a;
+    printf("Hello, %d\n",c);
     
-    printf("fdsafdsafds\n");
+     c = (test+1)->a;
+    printf("Hello, %d\n",c);
+    
+     c = (test+2)->a;
+    printf("Hello, %d\n",c);
+    
+    printf("Hello, BridgeCore Test!\n");
+    return 0;
 }
