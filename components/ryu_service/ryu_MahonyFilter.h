7225a157f9048eb05906ce4234c0e60609ed9088
taejun Ryu <ryuwillow@gmail.com>
2026-05-09 20:25:24 +0900

    ;;;;;;

--- components/ryu_service/ryu_MahonyFilter.h
+++ components/ryu_service/ryu_MahonyFilter.h
@@ -16,13 +16,9 @@ class Mahony{
         }
         Mahony(const Mahony&) = delete;
         Mahony& operator=(const Mahony&) = delete;
-        Mahony(Mahony&&) = delete;
-        Mahony& operator=(Mahony&&) = delete;
 
         const float INTEGRAL_MAX = 0.5f;
 
-
-        float q0, q1, q2, q3; // 쿼터니언
         float integralFBx = 0.0f, integralFBy = 0.0f, integralFBz = 0.0f; // 적분 오차
         void MahonyAHRSupdate(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, float dt);
         void calibrate_mahony_initial_attitude(float ax, float ay, float az, float mx, float my, float mz);
@@ -32,10 +28,10 @@ class Mahony{
         bool is_initialized(){return _initialized;};
 
     private:
+        float q0, q1, q2, q3; // 쿼터니언
         //초기 안정화를 빠르게하기 위한 변수
         bool is_booting = true;
-        uint64_t boot_start_time; // booting time
-    
+        uint64_t boot_start_time; // booting time    
         bool _initialized = false;
 };
 
